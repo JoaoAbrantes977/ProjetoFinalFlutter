13d2f33086be2c99b26c02419dfc8f3eb3ca4685
João Abrantes <101903253+JoaoAbrantes977@users.noreply.github.com>
2024-06-12 01:35:30 +0100

    create new inspection

--- lib/register.dart
+++ lib/register.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
-import 'package:projeto_final_flutter/login.dart';
+import 'login.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -13,14 +12,25 @@ class RegisterPageState extends State<RegisterPage> {
   final _formKey = GlobalKey<FormState>();
   String _selectedCountry = 'Portugal';
   String _selectedSpecialty = 'Operador';
+  String _selectDistrito = "Castelo Branco";
+  String _selectMunicipio = "Covilhã";
+  String _selectFreguesia = "Covilhã";
 
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _ruaController = TextEditingController();
+  final TextEditingController _codigoController = TextEditingController();
+  final TextEditingController _distritoController = TextEditingController();
+  final TextEditingController _municipioController = TextEditingController();
+  final TextEditingController _freguesiaController = TextEditingController();
 
   final List<String> _countries = ['Portugal', 'Spain', 'France', 'UK', 'USA'];
   final List<String> _specialties = ['Operador', 'Controlador', 'Inspetor'];
+  final List<String> _distrito = ['Castelo Branco', 'Lisboa', 'Porto'];
+  final List<String> _municipio = ['Covilhã', 'Fundão', 'Belmonte'];
+  final List<String> _freguesia = ['Dominguiso', 'Covilhã', 'Boidobra'];
 
   // FUNÇÃO DE REGISTO
   Future<void> _register() async {
@@ -29,6 +39,8 @@ class RegisterPageState extends State<RegisterPage> {
       final phone = _phoneController.text;
       final email = _emailController.text;
       final password = _passwordController.text;
+      final rua = _ruaController.text;
+      final codigo_postal = _codigoController.text;
 
       final url = Uri.parse('http://10.0.2.2:3000/user/register');
       final response = await http.post(
@@ -43,6 +55,11 @@ class RegisterPageState extends State<RegisterPage> {
           'password': password,
           'pais_nome': _selectedCountry,
           'especialidade': _selectedSpecialty,
+          'distrito_nome': _selectDistrito,
+          'municipio_nome': _selectMunicipio,
+          'freguesia_nome': _selectFreguesia,
+          'rua': rua,
+          'codigo_postal': codigo_postal
         }),
       );
 
@@ -55,7 +72,7 @@ class RegisterPageState extends State<RegisterPage> {
         );
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(builder: (context) =>  LoginPage()),
+          MaterialPageRoute(builder: (context) => LoginPage()),
         );
         print('Registration successful');
       } else if (response.statusCode == 409) {
@@ -99,32 +116,32 @@ class RegisterPageState extends State<RegisterPage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   const Text(
-                    'Register Page',
+                    'Registo',
                     style: TextStyle(fontSize: 24),
                   ),
-                  SizedBox(height: 20),
+                  const SizedBox(height: 20),
                   TextFormField(
                     controller: _nameController,
                     decoration: InputDecoration(
-                      labelText: 'Name',
-                      prefixIcon: Icon(Icons.person),
+                      labelText: 'Nome',
+                      prefixIcon: const Icon(Icons.person),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8),
                       ),
                     ),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
-                        return 'Name is required';
+                        return 'Campo Obrigatório';
                       }
                       return null;
                     },
                   ),
-                  SizedBox(height: 20),
+                  const SizedBox(height: 20),
                   TextFormField(
                     controller: _phoneController,
                     decoration: InputDecoration(
-                      labelText: 'Phone',
-                      prefixIcon: Icon(Icons.phone),
+                      labelText: 'Telefone',
+                      prefixIcon: const Icon(Icons.phone),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8),
                       ),
@@ -132,17 +149,17 @@ class RegisterPageState extends State<RegisterPage> {
                     keyboardType: TextInputType.phone,
                     validator: (value) {
                       if (value == null || value.isEmpty) {
-                        return 'Phone is required';
+                        return 'Campo Obrigatório';
                       }
                       return null;
                     },
                   ),
-                  SizedBox(height: 20),
+                  const SizedBox(height: 20),
                   TextFormField(
                     controller: _emailController,
                     decoration: InputDecoration(
                       labelText: 'Email',
-                      prefixIcon: Icon(Icons.email),
+                      prefixIcon: const Icon(Icons.email),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8),
                       ),
@@ -150,17 +167,35 @@ class RegisterPageState extends State<RegisterPage> {
                     keyboardType: TextInputType.emailAddress,
                     validator: (value) {
                       if (value == null || value.isEmpty) {
-                        return 'Email is required';
+                        return 'Campo Obrigatório';
                       }
                       // You can add email validation logic here if needed
                       return null;
                     },
                   ),
-                  SizedBox(height: 20),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      prefixIcon: const Icon(Icons.lock),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Password is required';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20),
                   DropdownButtonFormField<String>(
                     value: _selectedSpecialty,
                     decoration: InputDecoration(
-                      labelText: 'Specialty',
+                      labelText: 'Especialidade',
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8),
                       ),
@@ -181,7 +216,7 @@ class RegisterPageState extends State<RegisterPage> {
                   DropdownButtonFormField<String>(
                     value: _selectedCountry,
                     decoration: InputDecoration(
-                      labelText: 'Country',
+                      labelText: 'País',
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8),
                       ),
@@ -199,19 +234,104 @@ class RegisterPageState extends State<RegisterPage> {
                     },
                   ),
                   const SizedBox(height: 20),
-                  TextFormField(
-                    controller: _passwordController,
+                  DropdownButtonFormField<String>(
+                    value: _selectDistrito,
                     decoration: InputDecoration(
-                      labelText: 'Password',
-                      prefixIcon: Icon(Icons.lock),
+                      labelText: 'Distrito',
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8),
                       ),
                     ),
-                    obscureText: true,
+                    items: _distrito.map((String distrito) {
+                      return DropdownMenuItem<String>(
+                        value: distrito,
+                        child: Text(distrito),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectDistrito = newValue!;
+                      });
+                      // Update the selected district in the controller
+                      _distritoController.text = newValue!;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  DropdownButtonFormField<String>(
+                    value: _selectMunicipio,
+                    decoration: InputDecoration(
+                      labelText: 'Municipio',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    items: _municipio.map((String municipio) {
+                      return DropdownMenuItem<String>(
+                        value: municipio,
+                        child: Text(municipio),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectMunicipio = newValue!;
+                      });
+                      // Update the selected municipality in the controller
+                      _municipioController.text = newValue!;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  DropdownButtonFormField<String>(
+                    value: _selectFreguesia,
+                    decoration: InputDecoration(
+                      labelText: 'Freguesia',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    items: _freguesia.map((String freguesia) {
+                      return DropdownMenuItem<String>(
+                        value: freguesia,
+                        child: Text(freguesia),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectFreguesia = newValue!;
+                      });
+                      // Update the selected parish in the controller
+                      _freguesiaController.text = newValue!;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _ruaController,
+                    decoration: InputDecoration(
+                      labelText: 'Rua',
+                      prefixIcon: const Icon(Icons.location_city),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
-                        return 'Password is required';
+                        return 'Campo Obrigatório';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _codigoController,
+                    decoration: InputDecoration(
+                      labelText: 'Código Postal',
+                      prefixIcon: const Icon(Icons.markunread_mailbox),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Campo Obrigatório';
                       }
                       return null;
                     },
@@ -219,7 +339,7 @@ class RegisterPageState extends State<RegisterPage> {
                   const SizedBox(height: 20),
                   ElevatedButton(
                     onPressed: _register,
-                    child: const Text('Register'),
+                    child: const Text('Registar'),
                   ),
                 ],
               ),
